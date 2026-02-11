103b50dfb3fa336ab0b05826fbafd88c23a8806a
Nagaraj Hubli <nagarajhubli@gmail.com>
2026-02-11 17:15:38 +0530

    Scripts and tools: fix gen-manpages with non-bitcoind builds

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -9,30 +9,32 @@ BITCOINCLI=${BITCOINCLI:-$SRCDIR/bitcoin-cli}
 BITCOINTX=${BITCOINTX:-$SRCDIR/bitcoin-tx}
 BITCOINQT=${BITCOINQT:-$SRCDIR/qt/bitcoin-qt}
 
-[ ! -x "$BITCOIND" ] && echo "$BITCOIND not found or not executable." && exit 1
-
 cmds=""
+first_cmd=""
 for cmd in "$BITCOIND" "$BITCOINCLI" "$BITCOINTX" "$BITCOINQT"; do
   if [ -x "$cmd" ]; then
-    cmds="$cmds $cmd"
+    cmds="$cmds$cmd\n"
+    if [ -z "$first_cmd" ]; then
+      first_cmd="$cmd"
+    fi
   else
     echo "Skipping $cmd: not found or not executable."
   fi
 done
 
-[ -z "$cmds" ] && echo "No binaries found, nothing to do." && exit 1
+[ -z "$first_cmd" ] && echo "No binaries found, nothing to do." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-BTCVERSTR=$($BITCOIND --version | head -n1 | awk -F'[ -]' '{ print $6 }')
-BTCGITSUFFIX=$($BITCOIND --version | head -n1 | awk -F'[ -]' '{ print $7 }')
+BTCVERSTR=$("$first_cmd" --version | head -n1 | awk -F'[ -]' '{ print $6 }')
+BTCGITSUFFIX=$("$first_cmd" --version | head -n1 | awk -F'[ -]' '{ print $7 }')
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
 # but has different outcomes for bitcoin-qt and bitcoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$BITCOIND --version | sed -n '1!p' >> footer.h2m
+"$first_cmd" --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $cmds; do
+for cmd in $(printf '%b' "$cmds"); do
   cmdname="${cmd##*/}"
   help2man -N --version-string="$BTCVERSTR" --include=footer.h2m -o "${MANDIR}/${cmdname}.1" "$cmd"
   if [ -n "$BTCGITSUFFIX" ]; then
